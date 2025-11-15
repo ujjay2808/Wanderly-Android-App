9d46c2122b881c333d4a3760d6a79f7d106f90d7
ujjay2808 <ujjaymanety2808@gmail.com>
2025-11-15 10:58:59 +0530

    Create Constants.java

---
+++ app/src/main/java/com/ujjay/wanderly/utils/Constants.java
@@ -0,0 +1,13 @@
+package com.ujjay.wanderly.utils;
+
+public class Constants {
+    // API key will be provided via BuildConfig from local.properties
+    public static final String _GROQ_API_KEY = "YOUR_API_KEY";
+
+    public static final String _GROQ_API_URL = "https://api.groq.com/openai/v1/chat/completions";
+    public static final String _GROQ_MODEL = "llama-3.1-8b-instant";
+
+    // Add any other constants you had
+    public static final String APP_NAME = "Wanderly";
+    public static final String DATABASE_NAME = "wanderly.db";
+}
